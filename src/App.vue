a38844cd7643839c1837014f21da217362ed43a6
latermonk <iback.china@gmail.com>
2021-08-09 10:44:06 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,33 +5,10 @@
 
     <router-link class="list-group-item" active-class="active" to="/page1">page1</router-link>
     <br>
-
     <router-link class="list-group-item" active-class="active" to="/page2">page2</router-link>
 
+    <router-view></router-view>
   </div>
-
-<!--  <div class="row">-->
-<!--    <div class="col-xs-2 col-xs-offset-2">-->
-<!--      <div class="list-group">-->
-<!--        &lt;!&ndash; 原始html中我们使用a标签实现页面的跳转 &ndash;&gt;-->
-<!--        &lt;!&ndash; <a class="list-group-item active" href="./about.html">About</a> &ndash;&gt;-->
-<!--        &lt;!&ndash; <a class="list-group-item" href="./home.html">Home</a> &ndash;&gt;-->
-
-<!--        &lt;!&ndash; Vue中借助router-link标签实现路由的切换 &ndash;&gt;-->
-<!--        <router-link class="list-group-item" active-class="active" to="/about">About</router-link>-->
-<!--        <router-link class="list-group-item" active-class="active" to="/home">Home</router-link>-->
-<!--      </div>-->
-<!--    </div>-->
-<!--    <div class="col-xs-6">-->
-<!--      <div class="panel">-->
-<!--        <div class="panel-body">-->
-<!--          &lt;!&ndash; 指定组件的呈现位置 &ndash;&gt;-->
-<!--          <router-view></router-view>-->
-<!--        </div>-->
-<!--      </div>-->
-<!--    </div>-->
-<!--  </div>-->
-
 
 
 </template>
